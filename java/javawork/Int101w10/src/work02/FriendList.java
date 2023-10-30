2f4284b10aab19aee1cb7d7e8dad0db28bbc3802
nong540413 <142777913+nong540413@users.noreply.github.com>
2023-10-30 21:44:35 +0700

    Update FriendList.java

--- java/javawork/Int101w10/src/work02/FriendList.java
+++ java/javawork/Int101w10/src/work02/FriendList.java
@@ -1,40 +1,32 @@
-package work02;
-
-public class FriendList {
-    private final String name;
-    private FriendList next;
-    private FriendList(String name) { this.name = name; }
-    public static FriendList newList() { return new FriendList(""); }
-    public boolean addFriend(String name) {
-        /*
-        It returns "false" if "name" is null or a blank string or
-        this "name" has already existed its "name" field of itself,
-        its "next", or its "next" of "next" ...
-        Otherwise, it will create a new "FriendList" object with that "name"
-        and put it in the "next" field of the last "FriendList".
-        */
-        if(name == null || name.isBlank())return false;
-
-        var current = this;
-        while (current != null){
-            current = current.next;
-            if(current.name.equals(name)) return  false;
-
-        }
-        current.next = new FriendList(name);
-        return true;
-    }
-    @Override
-    public String toString() {
-        /* this method returns a "String" containing "name" of
-        all "FriendList" object in all the "next" fields */
-        var sb = new StringBuilder();
-        var current = this;
-        while (current.next != null){
-            sb.append(current.name).append(":");
-            current = current.next;
-
-        }
-        return sb.toString();
-    }
+package work02; //3.
+public class FriendList { //3.,3.1
+ private final String friend; //3.2
+ private FriendList next; //3.3
+ private FriendList(String friend) { //3.4
+ this.friend = friend;
+ }
+ public static FriendList newList() { //3.5
+ return new FriendList("");
+ }
+ public boolean addFriend(String friend) { //3.6
+ if (friend == null || friend.isBlank()) return false;
+ var current = this;
+ while (current.next != null) {
+ current = current.next;
+ if (current.friend.equals(friend)) return false;
+ }
+ current.next = new FriendList(friend);
+ return true;
+ }
+ @Override
+ public String toString() { //3.7
+ var current = this.next;
+ if (current == null) return "";
+ var s = new StringBuilder();
+ s.append(current.friend);
+ while ((current=current.next) != null) {
+ s.append(", ").append(current.friend);
+ }
+ return s.toString();
+ }
 }
